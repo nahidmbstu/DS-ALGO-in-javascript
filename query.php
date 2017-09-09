515d391c71e9caa8b549c436be4daaeac141f9ed
NahidMBSTU <nahid.mbstu.ict@gmail.com>
2017-09-09 16:22:31 +0600

    Update query.php

--- query.php
+++ query.php
@@ -2,14 +2,21 @@
 
 include 'dbcon.php';
 
-$name =isset($_GET['name'])?$_GET['name']:'';
+if (isset($_GET['name'])) {
 
-$query = mysqli_query( $conn,"SELECT * FROM movie WHERE name like '%$name%'");
+    $name = $_GET['name'];
+	
+// $name =isset($_GET['name'])?$_GET['name']:'';
+
+
+$query = mysqli_query( $conn,"SELECT * FROM movie WHERE name = '$name'");
 
 while($row = mysqli_fetch_array($query))
 {
     echo "<p>".$row['name']."</p>";
     echo "<p>".$row['year']."</p>";
+}
+
 }
 
 ?>
